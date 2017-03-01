c2f2f11d7b0d6a64a8f0dbdca33b429c7b97b6be
Daniel Lopez <danielopez582@gmail.com>
2017-02-28 17:50:52 -0700

    Class_catArticulos - Se ocultaron los campos CODIGO_UNIDAD_VENTA y NOMBRE_UNIDAD_VENTA

--- SIS_AGRO/Clases/Catalogos/Class_CatArticulos.vb
+++ SIS_AGRO/Clases/Catalogos/Class_CatArticulos.vb
@@ -32,8 +32,8 @@ Public Class Class_CatArticulos
     Private _CODIGO_BARRAS_PTI_13 As String = ""
     Private _CODIGO_BARRAS_PTI_14 As String = ""
     Private _ES_SERIALIZABLE As Boolean
-    Private _CODIGO_UNIDAD_VENTA As String
-    Private _NOMBRE_UNIDAD As String
+    'Private _CODIGO_UNIDAD_VENTA As String
+    'Private _NOMBRE_UNIDAD As String
 #End Region
 
 #Region "Campos ligados a la tabla"
@@ -265,23 +265,23 @@ Public Class Class_CatArticulos
         End Set
     End Property
 
-    Public Property CODIGO_UNIDAD_VENTA() As String
-        Get
-            Return Me._CODIGO_UNIDAD_VENTA
-        End Get
-        Set(ByVal VALUE As String)
-            Me._CODIGO_UNIDAD_VENTA = VALUE
-        End Set
-    End Property
+    'Public Property CODIGO_UNIDAD_VENTA() As String
+    '    Get
+    '        Return Me._CODIGO_UNIDAD_VENTA
+    '    End Get
+    '    Set(ByVal VALUE As String)
+    '        Me._CODIGO_UNIDAD_VENTA = VALUE
+    '    End Set
+    'End Property
 
-    Public Property NOMBRE_UNIDAD() As String
-        Get
-            Return Me._NOMBRE_UNIDAD
-        End Get
-        Set(ByVal VALUE As String)
-            Me._NOMBRE_UNIDAD = VALUE
-        End Set
-    End Property
+    'Public Property NOMBRE_UNIDAD() As String
+    '    Get
+    '        Return Me._NOMBRE_UNIDAD
+    '    End Get
+    '    Set(ByVal VALUE As String)
+    '        Me._NOMBRE_UNIDAD = VALUE
+    '    End Set
+    'End Property
 
 #End Region
 
@@ -399,7 +399,7 @@ Public Class Class_CatArticulos
             sqlParametro = .Parameters.Add("@CODIGO_ETIQUETA", SqlDbType.NVarChar, 2) : sqlParametro.Value = Me._CODIGO_ETIQUETA
             sqlParametro = .Parameters.Add("@RANGO_PIEZAS", SqlDbType.NVarChar, 20) : sqlParametro.Value = Me._RANGO_PIEZAS
             sqlParametro = .Parameters.Add("@ES_SERIALIZABLE", SqlDbType.Char, 1) : sqlParametro.Value = Convert.ToInt32(Me._ES_SERIALIZABLE)
-            sqlParametro = .Parameters.Add("@CODIGO_UNIDAD_VENTA", SqlDbType.NVarChar, 20) : sqlParametro.Value = Me._CODIGO_UNIDAD_VENTA.ToUpper
+            'sqlParametro = .Parameters.Add("@CODIGO_UNIDAD_VENTA", SqlDbType.NVarChar, 20) : sqlParametro.Value = Me._CODIGO_UNIDAD_VENTA.ToUpper
             sqlParametro = .Parameters.Add("@AGREGAR", SqlDbType.NVarChar, 1) : sqlParametro.Value = "1"
 
             Try
@@ -450,7 +450,7 @@ Public Class Class_CatArticulos
             sqlParametro = .Parameters.Add("@CODIGO_ETIQUETA", SqlDbType.NVarChar, 2) : sqlParametro.Value = Me._CODIGO_ETIQUETA
             sqlParametro = .Parameters.Add("@RANGO_PIEZAS", SqlDbType.NVarChar, 20) : sqlParametro.Value = Me._RANGO_PIEZAS
             sqlParametro = .Parameters.Add("@ES_SERIALIZABLE", SqlDbType.Char, 1) : sqlParametro.Value = Convert.ToInt32(Me._ES_SERIALIZABLE)
-            sqlParametro = .Parameters.Add("@CODIGO_UNIDAD_VENTA", SqlDbType.NVarChar, 20) : sqlParametro.Value = Me._CODIGO_UNIDAD_VENTA.ToUpper
+            'sqlParametro = .Parameters.Add("@CODIGO_UNIDAD_VENTA", SqlDbType.NVarChar, 20) : sqlParametro.Value = Me._CODIGO_UNIDAD_VENTA.ToUpper
             sqlParametro = .Parameters.Add("@AGREGAR", SqlDbType.NVarChar, 1) : sqlParametro.Value = "0"
 
             Try
