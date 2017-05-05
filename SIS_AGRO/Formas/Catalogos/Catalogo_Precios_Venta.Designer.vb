0ac643a677c451a427e2e05bd5e23f1c39bd1b3e
Jorge Gonzalez <luixstein@gmail.com>
2017-05-05 09:35:11 -0600

    Inicio de catálogo de precios de venta

---
+++ SIS_AGRO/Formas/Catalogos/Catalogo_Precios_Venta.Designer.vb
@@ -0,0 +1,59 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Catalogo_Precios_Venta
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Catalogo_Precios_Venta))
+        Me.Grid = New FlexCell.Grid()
+        Me.SuspendLayout()
+        '
+        'Grid
+        '
+        Me.Grid.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.Grid.CheckedImage = CType(resources.GetObject("Grid.CheckedImage"), System.Drawing.Bitmap)
+        Me.Grid.Cols = 1
+        Me.Grid.DefaultFont = New System.Drawing.Font("Tahoma", 8.25!)
+        Me.Grid.DisplayRowNumber = True
+        Me.Grid.FixedRowColStyle = FlexCell.FixedRowColStyleEnum.VisualStyles
+        Me.Grid.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Grid.GridColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Grid.Location = New System.Drawing.Point(12, 35)
+        Me.Grid.LockButton = True
+        Me.Grid.Name = "Grid"
+        Me.Grid.Rows = 1
+        Me.Grid.Size = New System.Drawing.Size(767, 355)
+        Me.Grid.TabIndex = 351
+        Me.Grid.UncheckedImage = CType(resources.GetObject("Grid.UncheckedImage"), System.Drawing.Bitmap)
+        '
+        'Catalogo_Precios_Venta
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(801, 399)
+        Me.Controls.Add(Me.Grid)
+        Me.Name = "Catalogo_Precios_Venta"
+        Me.Text = "Catalogo_Precios_Venta"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Grid As FlexCell.Grid
+End Class
---
+++ SIS_AGRO/Formas/Catalogos/Catalogo_Precios_Venta.vb
@@ -0,0 +1,22 @@
+﻿Option Strict On
+
+Public Class Catalogo_Precios_Venta
+
+#Region "Columnas grid"
+
+#End Region
+
+#Region "Eventos de objetos"
+#Region "Eventos"
+
+#End Region
+#Region "Eventos Genericos"
+
+#End Region
+#End Region
+
+#Region "Métodos y procedimientos"
+
+#End Region
+
+End Class
