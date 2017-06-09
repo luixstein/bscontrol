9068735c562b2c6285d19bc69f1836c1a3df5a10
Jorge Gonzalez <luixstein@gmail.com>
2017-06-09 17:19:41 -0600

    142, Fix menor mostra versión db vs exe

--- SIS_AGRO/Formas/Sistema/LoginForm.vb
+++ SIS_AGRO/Formas/Sistema/LoginForm.vb
@@ -243,7 +243,8 @@ Public Class LoginForm
         Plaza = New Class_SisPlazas
 
         If Empresa_Sistema.VERSION_AGROCONTROL <> My.Application.Info.Version.Revision Then
-            MsgBox("La versión no esta actualizada. Version Bd: " & Empresa_Sistema.VERSION_AGROCONTROL & " Versión aplicación: " & My.Application.Info.Version.Revision, MsgBoxStyle.Exclamation, Me.Text)
+            MsgBox("La versión no esta actualizada. Version Basedatos: " & Empresa_Sistema.VERSION_AGROCONTROL & " Versión aplicación: " & My.Application.Info.Version.Revision, MsgBoxStyle.Exclamation,
+                   "Servidor=" & My.Settings.Servidor & " Basedatos=" & My.Settings.BaseDatos)
             Finaliza(False)
             Exit Sub
         End If
--- SIS_AGRO/My Project/AssemblyInfo.vb
+++ SIS_AGRO/My Project/AssemblyInfo.vb
@@ -31,6 +31,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.141")> 
-<Assembly: AssemblyFileVersion("1.0.0.141")> 
+<Assembly: AssemblyVersion("1.0.0.142")>
+<Assembly: AssemblyFileVersion("1.0.0.142")>
 
