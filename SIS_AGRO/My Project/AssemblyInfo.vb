535e937b9d521f926d4bd3355f8b8808fdadff4d
Jorge Gonzalez <luixstein@gmail.com>
2017-08-07 12:39:28 -0600

    160 test kits

--- SIS_AGRO/My Project/AssemblyInfo.vb
+++ SIS_AGRO/My Project/AssemblyInfo.vb
@@ -31,6 +31,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.151")>
-<Assembly: AssemblyFileVersion("1.0.0.151")>
+<Assembly: AssemblyVersion("1.0.0.160")>
+<Assembly: AssemblyFileVersion("1.0.0.160")>
 
