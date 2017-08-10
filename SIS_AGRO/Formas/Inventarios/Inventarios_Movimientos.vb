6de61c03302e48544e2d810c40722e9f0700174c
Daniel Lopez <danielopez582@gmail.com>
2017-08-10 17:40:35 -0600

    Inventarios movimientos - fix en CantidadArticulosPendientesSerie

--- SIS_AGRO/Formas/Inventarios/Inventarios_Movimientos.vb
+++ SIS_AGRO/Formas/Inventarios/Inventarios_Movimientos.vb
@@ -2163,7 +2163,7 @@ busca_serie:
         Dim iArticulosEncontrados As Integer = 0
         Try
             For i = 1 To Me.GridSeries.Rows - 1
-                If Me.GridSeries.Cell(i, Me.igySerieCodigo).Text = sCodigoArticulo AndAlso Me.Grid1.Cell(i, Me.iGyCodigo).Text <> "-" AndAlso txtLEN(Me.GridSeries.Cell(i, Me.igySerieIdInventarioLotesCostos).Text) = False Then
+                If Me.GridSeries.Cell(i, Me.igySerieCodigo).Text = sCodigoArticulo AndAlso Me.GridSeries.Cell(i, Me.iGyCodigo).Text <> "-" AndAlso txtLEN(Me.GridSeries.Cell(i, Me.igySerieIdInventarioLotesCostos).Text) = False Then
                     iArticulosEncontrados += 1
                 End If
             Next
