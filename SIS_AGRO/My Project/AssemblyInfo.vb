a56965392aaab68d5ac035ee9ebc78f29616d1b4
Daniel Lopez <danielopez582@gmail.com>
2018-08-09 17:11:12 -0600

    229 - class ventas global fix

--- SIS_AGRO/My Project/AssemblyInfo.vb
+++ SIS_AGRO/My Project/AssemblyInfo.vb
@@ -31,6 +31,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.228")> 
-<Assembly: AssemblyFileVersion("1.0.0.228")> 
+<Assembly: AssemblyVersion("1.0.0.229")> 
+<Assembly: AssemblyFileVersion("1.0.0.229")> 
 
