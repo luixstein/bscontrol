22e335cd24a0b62439f5faaf1ae4011430773560
Jorge Gonzalez <luixstein@gmail.com>
2018-08-21 15:53:00 -0600

    232, fix faltaba grabar la serie en pagos cfdi

--- SIS_AGRO/My Project/AssemblyInfo.vb
+++ SIS_AGRO/My Project/AssemblyInfo.vb
@@ -31,6 +31,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.231")> 
-<Assembly: AssemblyFileVersion("1.0.0.231")> 
+<Assembly: AssemblyVersion("1.0.0.232")>
+<Assembly: AssemblyFileVersion("1.0.0.232")>
 
