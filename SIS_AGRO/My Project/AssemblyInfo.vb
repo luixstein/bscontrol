451ec0bd6c97f629ff0ac1ed3fe095a461d354c2
Daniel Lopez <danielopez582@gmail.com>
2020-02-25 15:44:17 -0700

    297 -FIx compras, en busqueda de articulos con f6 solo mostraba articulos con existencia

--- SIS_AGRO/My Project/AssemblyInfo.vb
+++ SIS_AGRO/My Project/AssemblyInfo.vb
@@ -31,6 +31,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.296")> 
-<Assembly: AssemblyFileVersion("1.0.0.296")> 
+<Assembly: AssemblyVersion("1.0.0.297")> 
+<Assembly: AssemblyFileVersion("1.0.0.297")> 
 
