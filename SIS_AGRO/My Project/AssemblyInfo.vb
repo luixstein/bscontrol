5de69fbb681f63c504a09c1b411f82d77a2431eb
Daniel Lopez <danielopez582@gmail.com>
2020-04-28 17:13:17 -0600

    316

--- SIS_AGRO/My Project/AssemblyInfo.vb
+++ SIS_AGRO/My Project/AssemblyInfo.vb
@@ -31,6 +31,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.315")> 
-<Assembly: AssemblyFileVersion("1.0.0.315")> 
+<Assembly: AssemblyVersion("1.0.0.316")> 
+<Assembly: AssemblyFileVersion("1.0.0.316")> 
 
