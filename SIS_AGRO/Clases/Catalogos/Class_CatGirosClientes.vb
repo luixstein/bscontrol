8f69dd28468475444337d0a0e3b51275e291ecc8
Daniel Lopez <danielopez582@gmail.com>
2020-05-26 00:20:31 -0600

    cat giros clientes  y codigo_giro en cat_clientes

---
+++ SIS_AGRO/Clases/Catalogos/Class_CatGirosClientes.vb
@@ -0,0 +1,318 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+
+Public Class Class_CatGirosClientes
+    Inherits Class_Catalogos
+
+#Region "Campos"
+
+#Region "Campos de la tabla"
+    Private _CODIGO_GIRO As Integer
+    Private _NOMBRE_GIRO As String
+#End Region
+
+#Region "Campos ligados a la tabla"
+    Private _Existe As Boolean
+#End Region
+
+#Region "Campos públicos"
+
+#End Region
+
+#Region "Campos privados"
+
+#End Region
+
+#Region "Campos de sistema"
+    Private _Nombre_Catalogo As String
+    Private _Nombre_Reporte As String
+    Private _Conexion As SqlConnection
+    Private _QuerySelect As String
+    Private _QueryOrder As String
+
+#End Region
+
+#End Region
+
+#Region "Propiedades"
+
+#Region "Propiedades Campos de la tabla"
+    Public Property CODIGO_GIRO() As Integer
+        Get
+            Return Me._CODIGO_GIRO
+        End Get
+        Set(ByVal Value As Integer)
+            Me._CODIGO_GIRO = Value
+        End Set
+    End Property
+
+    Public Property NOMBRE_GIRO() As String
+        Get
+            Return Me._NOMBRE_GIRO
+        End Get
+        Set(ByVal Value As String)
+            Me._NOMBRE_GIRO = Value
+        End Set
+    End Property
+#End Region
+
+#Region "Propiedades de campos ligados a la tabla"
+    Public ReadOnly Property Existe() As Boolean
+        Get
+            Return Me._Existe
+        End Get
+    End Property
+
+#End Region
+
+#Region "Propiedades públicos"
+
+#End Region
+
+#Region "Propiedades de campos privados"
+
+#End Region
+#Region "Propiedades de campos de sistema"
+
+    Public Overrides ReadOnly Property Nombre_Catalogo() As String
+        Get
+            Return Me._Nombre_Catalogo
+        End Get
+    End Property
+
+    Public Overrides Property Nombre_Reporte() As String
+        Get
+            Return Me._Nombre_Reporte
+        End Get
+        Set(ByVal value As String)
+            Me._Nombre_Reporte = value
+        End Set
+    End Property
+    'Public Property Estatus() As String
+    '    Get
+    '        Return Me._Estatus
+    '    End Get
+    '    Set(ByVal value As String)
+    '        Me._Estatus = value
+    '    End Set
+    'End Property
+
+#End Region
+
+#End Region
+
+#Region "Constructor y destructor"
+
+    Public Sub New()
+        Me._Nombre_Catalogo = "CAT_GIROS_CLIENTES"
+        Me._Nombre_Reporte = "RPT_CAT_GIROS_CLIENTES"
+        Me._Conexion = New SqlConnection
+        Me._Conexion.ConnectionString = Empresa_Sistema.conexion
+        Me._QuerySelect = "Select Codigo_giro,Nombre_giro From CAT_GIROS_CLIENTES"
+        Me._QueryOrder = " Order by Nombre_giro"
+    End Sub                                                         'Inicializa al objeto.
+
+    Public Sub New(ByVal sCodigoGiro As String)
+        Me.New()
+        Try
+            Me._CODIGO_GIRO = sCodigoGiro
+            If Me.Consultar = True Then
+                Me._Existe = True
+
+            End If
+        Catch ex As Exception
+            HandleError(Me._Nombre_Catalogo, "New", ex)
+        End Try
+    End Sub
+
+    Protected Overrides Sub Finalize()
+        'Me._Conexion.Dispose()
+        MyBase.Finalize()
+    End Sub
+#End Region
+
+#Region "Opciones"
+
+#End Region
+
+#Region "Métodos y procedimientos"
+
+    Public Overrides Function Insertar() As Boolean
+        'Dim bResultado As Boolean = False
+        'Dim cmd As New SqlCommand
+        'Dim sqlParametro As SqlParameter
+        'With cmd
+        '    .Connection = Me._Conexion
+        '    .CommandTimeout = 0
+        '    .CommandType = CommandType.StoredProcedure
+        '    .CommandText = "MP_CAT_CONCEPTOS_GRABA"
+
+        '    sqlParametro = .Parameters.Add("@CODIGO_CONCEPTO", SqlDbType.SmallInt) : sqlParametro.Value = CInt(Me._Codigo_Concepto) : sqlParametro.Direction = ParameterDirection.InputOutput
+        '    sqlParametro = .Parameters.Add("@NOMBRE_CONCEPTO", SqlDbType.NVarChar, 200) : sqlParametro.Value = Me._Nombre_Concepto.ToString.ToUpper
+        '    sqlParametro = .Parameters.Add("@ESTATUS", SqlDbType.Char, 1) : sqlParametro.Value = Me.Estatus.ToString.ToUpper
+        '    sqlParametro = .Parameters.Add("@ACCION", SqlDbType.NVarChar, 20) : sqlParametro.Value = "INSERTAR"
+        '    Try
+        '        Me._Conexion.Open()
+        '        .ExecuteNonQuery()
+        '        Me._Codigo_Concepto = .Parameters("@CODIGO_CONCEPTO").Value.ToString
+        '        bResultado = True
+        '    Catch ex As Exception
+        '        HandleError(Me._Nombre_Catalogo, "Insertar", ex)
+        '    Finally
+        '        Me._Conexion.Close()
+        '        cmd.Dispose()
+        '        sqlParametro = Nothing
+        '    End Try
+        'End With
+        'Return bResultado
+    End Function
+
+    Public Overrides Function Actualizar() As Boolean
+        'Dim bResultado As Boolean = False
+        'Dim cmd As New SqlCommand
+        'Dim sqlParametro As SqlParameter
+        'With cmd
+        '    .Connection = Me._Conexion
+        '    .CommandTimeout = 0
+        '    .CommandType = CommandType.StoredProcedure
+        '    .CommandText = "MP_CAT_CONCEPTOS_GRABA"
+
+        '    sqlParametro = .Parameters.Add("@CODIGO_CONCEPTO", SqlDbType.SmallInt) : sqlParametro.Value = CInt(Me._Codigo_Concepto)
+        '    sqlParametro = .Parameters.Add("@NOMBRE_CONCEPTO", SqlDbType.NVarChar, 200) : sqlParametro.Value = Me._Nombre_Concepto.ToString.ToUpper
+        '    sqlParametro = .Parameters.Add("@ESTATUS", SqlDbType.Char, 1) : sqlParametro.Value = Me.Estatus.ToString.ToUpper
+        '    sqlParametro = .Parameters.Add("@ACCION", SqlDbType.NVarChar, 20) : sqlParametro.Value = "ACTUALIZAR"
+        '    Try
+        '        Me._Conexion.Open()
+        '        .ExecuteNonQuery()
+        '        bResultado = True
+        '    Catch ex As Exception
+        '        HandleError(Me._Nombre_Catalogo, "Actualizar", ex)
+        '    Finally
+        '        Me._Conexion.Close()
+        '        cmd.Dispose()
+        '        sqlParametro = Nothing
+        '    End Try
+        'End With
+        'Return bResultado
+    End Function
+
+    Public Overrides Function Consultar() As Boolean
+        Dim bResultado As Boolean = False
+        Dim cmd As New SqlCommand("SELECT * FROM CAT_GIROS_CLIENTES WHERE CODIGO_GIRO ='" & Replace(Me._CODIGO_GIRO, "'", "''") & "'", Me._Conexion)
+        Dim dReader As SqlDataReader
+        With cmd
+            .CommandTimeout = 0
+            .CommandType = CommandType.Text
+            Try
+                Me._Conexion.Open()
+                dReader = .ExecuteReader()
+
+                If dReader.Read Then
+                    Me._CODIGO_GIRO = "" & dReader("CODIGO_GIRO").ToString
+                    Me._NOMBRE_GIRO = Trim("" & dReader("NOMBRE_GIRO").ToString)
+                    bResultado = True
+                End If
+                dReader.Close()
+            Catch ex As Exception
+                HandleError(Me._Nombre_Catalogo, "Consultar", ex)
+            Finally
+                Me._Conexion.Close()
+                cmd.Dispose()
+            End Try
+        End With
+        Return bResultado
+    End Function
+
+    Public Overrides Function ObtenerElementos() As System.Data.DataTable
+        Dim dTable As New DataTable
+        Dim da As New SqlDataAdapter(Me._QuerySelect & Me._QueryOrder, Me._Conexion)
+        Try
+            da.Fill(dTable)
+        Catch ex As Exception
+            HandleError(Me._Nombre_Catalogo, "ObtenerElementos", ex)
+        Finally
+            da.Dispose()
+        End Try
+        Return dTable
+    End Function
+
+    Public Function ObtenerElementosParaReportes() As System.Data.DataTable
+        Dim dTable As New DataTable
+        Dim da As New SqlDataAdapter(Me._QuerySelect & Me._QueryOrder, Me._Conexion)
+        Try
+            da.Fill(dTable)
+            dTable.Rows.Add("-1", "TODOS")
+        Catch ex As Exception
+            HandleError(Me._Nombre_Catalogo, "ObtenerElementosParaReportes", ex)
+        Finally
+            da.Dispose()
+        End Try
+        Return dTable
+    End Function
+
+    Public Function ObtenerElementosFiltro(ByVal Filtro As String) As System.Data.DataTable
+        Dim dTable As New DataTable
+        Dim da As New SqlDataAdapter("SELECT CODIGO_GIRO, NOMBRE_GIRO FROM CAT_GIROS_CLIENTES WHERE NOMBRE_GIRO LIKE '" & Filtro.ToString & "%' ORDER BY NOMBRE_GIRO", Me._Conexion)
+        Try
+            da.Fill(dTable)
+        Catch ex As Exception
+            HandleError(Me._Nombre_Catalogo, "ObtenerElementosFiltro", ex)
+        Finally
+            da.Dispose()
+        End Try
+        Return dTable
+    End Function
+
+    Public Overrides Function BusquedaVisual_PorCodigo() As String
+        Dim f As New BusquedaVisual
+        Dim Resultado As String = ""
+        f.Text = "Búsqueda de giros por codigo."
+        f.sCampo = "CODIGO_GIRO"
+        f.sOrder = "NOMBRE_GIRO"
+        f.sTable = "CAT_GIROS_CLIENTES"
+        f.sQl = "Select CODIGO_GIRO,NOMBRE_GIRO From CAT_GIROS_CLIENTES Where 1=1 And"
+        f.Inicia("")
+        f.ShowDialog()
+        Try
+            If f.iRows > 0 Then
+                Resultado = CType(f.GridBusqueda.Item(f.GridBusqueda.CurrentCell.RowNumber, 0), String)
+            End If
+        Catch ex As Exception
+            HandleError(Me.Nombre_Catalogo, "BusquedaVisual_PorCodigo", ex)
+        End Try
+        Return Resultado
+    End Function
+
+    Public Overrides Function BusquedaVisual_PorDescripcion() As String
+        Dim f As New BusquedaVisual
+        Dim Resultado As String = ""
+        f.Text = "Búsqueda de giros por nombre."
+        f.sCampo = "NOMBRE_GIRO"
+        f.sOrder = "NOMBRE_GIRO"
+        f.sTable = "CAT_GIROS_CLIENTES"
+        f.sQl = "Select CODIGO_GIRO,NOMBRE_GIRO From CAT_GIROS_CLIENTES Where 1=1 AND "
+        f.Inicia("")
+        f.ShowDialog()
+        Try
+            If f.iRows > 0 Then
+                Resultado = CType(f.GridBusqueda.Item(f.GridBusqueda.CurrentCell.RowNumber, 0), String)
+            End If
+        Catch ex As Exception
+            HandleError(Me.Nombre_Catalogo, "BusquedaVisual_PorDescripcion", ex)
+        End Try
+        Return Resultado
+    End Function
+
+    Public Function CodigoSiguiente() As String
+        Dim Resultado As Integer
+        Try
+            Dim sql As New Class_find("SELECT MAX(CODIGO_GIRO) FROM CAT_GIROS_CLIENTES")
+            Resultado = CType(sql.Result1, Integer) + 1
+        Catch ex As Exception
+            HandleError(Me.Nombre_Catalogo, "CodigoSiguiente", ex)
+        End Try
+        Return Resultado
+    End Function
+#End Region
+
+End Class
