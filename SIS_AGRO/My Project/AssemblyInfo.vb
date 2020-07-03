ea7c63e998f40de8ea9b731bf45f5396aac40c59
Daniel Lopez <danielopez582@gmail.com>
2020-07-03 14:58:35 -0600

    331 - Requisiciones de inventario

--- SIS_AGRO/My Project/AssemblyInfo.vb
+++ SIS_AGRO/My Project/AssemblyInfo.vb
@@ -31,6 +31,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.330")>
-<Assembly: AssemblyFileVersion("1.0.0.330")>
+<Assembly: AssemblyVersion("1.0.0.331")> 
+<Assembly: AssemblyFileVersion("1.0.0.331")> 
 
