59701cfdc2c699fdac88757b769d08634a83bb44
Daniel Lopez <danielopez582@gmail.com>
2020-12-21 23:04:14 -0700

    382 - Compras no inventariables fix error al pasar oc a compra

--- SIS_AGRO/My Project/AssemblyInfo.vb
+++ SIS_AGRO/My Project/AssemblyInfo.vb
@@ -31,6 +31,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.381")> 
-<Assembly: AssemblyFileVersion("1.0.0.381")> 
+<Assembly: AssemblyVersion("1.0.0.382")> 
+<Assembly: AssemblyFileVersion("1.0.0.382")> 
 
