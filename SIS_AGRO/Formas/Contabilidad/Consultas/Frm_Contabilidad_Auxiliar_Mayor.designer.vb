85afe312f01a7ef4f43e315f467d486bd821a808
Jorge Gonzalez <luixstein@gmail.com>
2021-05-14 16:04:30 -0600

    Rpt aux de mayor, se hizo mas larga la forma

--- SIS_AGRO/Formas/Contabilidad/Consultas/Frm_Contabilidad_Auxiliar_Mayor.designer.vb
+++ SIS_AGRO/Formas/Contabilidad/Consultas/Frm_Contabilidad_Auxiliar_Mayor.designer.vb
@@ -52,7 +52,7 @@ Partial Class Frm_Contabilidad_Auxiliar_Mayor
         Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tsbConsultar, Me.tsbImprimir, Me.tsbSalir})
         Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
         Me.ToolStrip1.Name = "ToolStrip1"
-        Me.ToolStrip1.Size = New System.Drawing.Size(1471, 27)
+        Me.ToolStrip1.Size = New System.Drawing.Size(1103, 27)
         Me.ToolStrip1.TabIndex = 2
         Me.ToolStrip1.Text = "ToolStrip1"
         '
@@ -61,7 +61,7 @@ Partial Class Frm_Contabilidad_Auxiliar_Mayor
         Me.tsbConsultar.Image = CType(resources.GetObject("tsbConsultar.Image"), System.Drawing.Image)
         Me.tsbConsultar.ImageTransparentColor = System.Drawing.Color.Magenta
         Me.tsbConsultar.Name = "tsbConsultar"
-        Me.tsbConsultar.Size = New System.Drawing.Size(95, 24)
+        Me.tsbConsultar.Size = New System.Drawing.Size(82, 24)
         Me.tsbConsultar.Text = "&Consultar"
         '
         'tsbImprimir
@@ -69,7 +69,7 @@ Partial Class Frm_Contabilidad_Auxiliar_Mayor
         Me.tsbImprimir.Image = CType(resources.GetObject("tsbImprimir.Image"), System.Drawing.Image)
         Me.tsbImprimir.ImageTransparentColor = System.Drawing.Color.Magenta
         Me.tsbImprimir.Name = "tsbImprimir"
-        Me.tsbImprimir.Size = New System.Drawing.Size(90, 24)
+        Me.tsbImprimir.Size = New System.Drawing.Size(77, 24)
         Me.tsbImprimir.Text = "&Imprimir"
         '
         'tsbSalir
@@ -77,87 +77,79 @@ Partial Class Frm_Contabilidad_Auxiliar_Mayor
         Me.tsbSalir.Image = CType(resources.GetObject("tsbSalir.Image"), System.Drawing.Image)
         Me.tsbSalir.ImageTransparentColor = System.Drawing.Color.Magenta
         Me.tsbSalir.Name = "tsbSalir"
-        Me.tsbSalir.Size = New System.Drawing.Size(62, 24)
+        Me.tsbSalir.Size = New System.Drawing.Size(53, 24)
         Me.tsbSalir.Text = "&Salir"
         '
         'LblEjercicio
         '
         Me.LblEjercicio.AutoSize = True
-        Me.LblEjercicio.Location = New System.Drawing.Point(21, 20)
-        Me.LblEjercicio.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.LblEjercicio.Location = New System.Drawing.Point(16, 16)
         Me.LblEjercicio.Name = "LblEjercicio"
-        Me.LblEjercicio.Size = New System.Drawing.Size(69, 17)
+        Me.LblEjercicio.Size = New System.Drawing.Size(53, 13)
         Me.LblEjercicio.TabIndex = 210
         Me.LblEjercicio.Text = "Ejercicio :"
         '
         'LblDisplayNombreSocio
         '
         Me.LblDisplayNombreSocio.AutoSize = True
-        Me.LblDisplayNombreSocio.Location = New System.Drawing.Point(21, 118)
-        Me.LblDisplayNombreSocio.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.LblDisplayNombreSocio.Location = New System.Drawing.Point(16, 96)
         Me.LblDisplayNombreSocio.Name = "LblDisplayNombreSocio"
-        Me.LblDisplayNombreSocio.Size = New System.Drawing.Size(61, 17)
+        Me.LblDisplayNombreSocio.Size = New System.Drawing.Size(47, 13)
         Me.LblDisplayNombreSocio.TabIndex = 209
         Me.LblDisplayNombreSocio.Text = "Cuenta :"
         '
         'TxtCuenta1
         '
-        Me.TxtCuenta1.Location = New System.Drawing.Point(135, 114)
-        Me.TxtCuenta1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.TxtCuenta1.Location = New System.Drawing.Point(101, 93)
         Me.TxtCuenta1.MaxLength = 15
         Me.TxtCuenta1.Name = "TxtCuenta1"
-        Me.TxtCuenta1.Size = New System.Drawing.Size(176, 22)
+        Me.TxtCuenta1.Size = New System.Drawing.Size(133, 20)
         Me.TxtCuenta1.TabIndex = 3
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(21, 90)
-        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Location = New System.Drawing.Point(16, 73)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(53, 17)
+        Me.Label1.Size = New System.Drawing.Size(41, 13)
         Me.Label1.TabIndex = 208
         Me.Label1.Text = "Hasta :"
         '
         'DtFechaHasta
         '
         Me.DtFechaHasta.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
-        Me.DtFechaHasta.Location = New System.Drawing.Point(135, 82)
-        Me.DtFechaHasta.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.DtFechaHasta.Location = New System.Drawing.Point(101, 67)
         Me.DtFechaHasta.MinDate = New Date(1900, 1, 1, 0, 0, 0, 0)
         Me.DtFechaHasta.Name = "DtFechaHasta"
-        Me.DtFechaHasta.Size = New System.Drawing.Size(176, 22)
+        Me.DtFechaHasta.Size = New System.Drawing.Size(133, 20)
         Me.DtFechaHasta.TabIndex = 2
         Me.DtFechaHasta.Value = New Date(2009, 9, 26, 0, 0, 0, 0)
         '
         'LblDisplayFechaNacimiento
         '
         Me.LblDisplayFechaNacimiento.AutoSize = True
-        Me.LblDisplayFechaNacimiento.Location = New System.Drawing.Point(21, 57)
-        Me.LblDisplayFechaNacimiento.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.LblDisplayFechaNacimiento.Location = New System.Drawing.Point(16, 46)
         Me.LblDisplayFechaNacimiento.Name = "LblDisplayFechaNacimiento"
-        Me.LblDisplayFechaNacimiento.Size = New System.Drawing.Size(57, 17)
+        Me.LblDisplayFechaNacimiento.Size = New System.Drawing.Size(44, 13)
         Me.LblDisplayFechaNacimiento.TabIndex = 207
         Me.LblDisplayFechaNacimiento.Text = "Desde :"
         '
         'DtFechaDesde
         '
         Me.DtFechaDesde.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
-        Me.DtFechaDesde.Location = New System.Drawing.Point(135, 49)
-        Me.DtFechaDesde.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.DtFechaDesde.Location = New System.Drawing.Point(101, 40)
         Me.DtFechaDesde.MinDate = New Date(1900, 1, 1, 0, 0, 0, 0)
         Me.DtFechaDesde.Name = "DtFechaDesde"
-        Me.DtFechaDesde.Size = New System.Drawing.Size(176, 22)
+        Me.DtFechaDesde.Size = New System.Drawing.Size(133, 20)
         Me.DtFechaDesde.TabIndex = 1
         Me.DtFechaDesde.Value = New Date(2009, 9, 26, 0, 0, 0, 0)
         '
         'LblCuenta
         '
         Me.LblCuenta.AutoSize = True
-        Me.LblCuenta.Location = New System.Drawing.Point(320, 118)
-        Me.LblCuenta.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.LblCuenta.Location = New System.Drawing.Point(240, 96)
         Me.LblCuenta.Name = "LblCuenta"
-        Me.LblCuenta.Size = New System.Drawing.Size(12, 17)
+        Me.LblCuenta.Size = New System.Drawing.Size(10, 13)
         Me.LblCuenta.TabIndex = 211
         Me.LblCuenta.Text = "."
         '
@@ -165,11 +157,10 @@ Partial Class Frm_Contabilidad_Auxiliar_Mayor
         '
         Me.CmbEjercicio.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.CmbEjercicio.FormattingEnabled = True
-        Me.CmbEjercicio.Location = New System.Drawing.Point(135, 15)
-        Me.CmbEjercicio.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.CmbEjercicio.Location = New System.Drawing.Point(101, 12)
         Me.CmbEjercicio.MaxLength = 1
         Me.CmbEjercicio.Name = "CmbEjercicio"
-        Me.CmbEjercicio.Size = New System.Drawing.Size(176, 24)
+        Me.CmbEjercicio.Size = New System.Drawing.Size(133, 21)
         Me.CmbEjercicio.TabIndex = 0
         '
         'GroupBox1
@@ -184,34 +175,30 @@ Partial Class Frm_Contabilidad_Auxiliar_Mayor
         Me.GroupBox1.Controls.Add(Me.Label1)
         Me.GroupBox1.Controls.Add(Me.LblDisplayNombreSocio)
         Me.GroupBox1.Controls.Add(Me.TxtCuenta1)
-        Me.GroupBox1.Location = New System.Drawing.Point(16, 34)
-        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 28)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.GroupBox1.Size = New System.Drawing.Size(1439, 153)
+        Me.GroupBox1.Size = New System.Drawing.Size(1079, 124)
         Me.GroupBox1.TabIndex = 0
         Me.GroupBox1.TabStop = False
         '
         'cbkFiltoContraPolizas
         '
         Me.cbkFiltoContraPolizas.AutoSize = True
-        Me.cbkFiltoContraPolizas.Location = New System.Drawing.Point(349, 20)
-        Me.cbkFiltoContraPolizas.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.cbkFiltoContraPolizas.Location = New System.Drawing.Point(262, 16)
         Me.cbkFiltoContraPolizas.Name = "cbkFiltoContraPolizas"
-        Me.cbkFiltoContraPolizas.Size = New System.Drawing.Size(164, 21)
+        Me.cbkFiltoContraPolizas.Size = New System.Drawing.Size(125, 17)
         Me.cbkFiltoContraPolizas.TabIndex = 212
-        Me.cbkFiltoContraPolizas.Text = "Ocultar contrapolizas"
+        Me.cbkFiltoContraPolizas.Text = "Ocultar contrapólizas"
         Me.cbkFiltoContraPolizas.UseVisualStyleBackColor = True
         Me.cbkFiltoContraPolizas.Visible = False
         '
         'txtTotalCargos
         '
         Me.txtTotalCargos.BackColor = System.Drawing.SystemColors.Control
-        Me.txtTotalCargos.Location = New System.Drawing.Point(1035, 630)
-        Me.txtTotalCargos.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.txtTotalCargos.Location = New System.Drawing.Point(776, 651)
         Me.txtTotalCargos.MaxLength = 15
         Me.txtTotalCargos.Name = "txtTotalCargos"
-        Me.txtTotalCargos.Size = New System.Drawing.Size(119, 22)
+        Me.txtTotalCargos.Size = New System.Drawing.Size(90, 20)
         Me.txtTotalCargos.TabIndex = 219
         Me.txtTotalCargos.Text = "0.00"
         Me.txtTotalCargos.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
@@ -219,11 +206,10 @@ Partial Class Frm_Contabilidad_Auxiliar_Mayor
         'txtTotalAbonos
         '
         Me.txtTotalAbonos.BackColor = System.Drawing.SystemColors.Control
-        Me.txtTotalAbonos.Location = New System.Drawing.Point(1163, 630)
-        Me.txtTotalAbonos.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.txtTotalAbonos.Location = New System.Drawing.Point(872, 651)
         Me.txtTotalAbonos.MaxLength = 15
         Me.txtTotalAbonos.Name = "txtTotalAbonos"
-        Me.txtTotalAbonos.Size = New System.Drawing.Size(119, 22)
+        Me.txtTotalAbonos.Size = New System.Drawing.Size(90, 20)
         Me.txtTotalAbonos.TabIndex = 220
         Me.txtTotalAbonos.Text = "0.00"
         Me.txtTotalAbonos.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
@@ -231,11 +217,10 @@ Partial Class Frm_Contabilidad_Auxiliar_Mayor
         'txtSaldoTotal
         '
         Me.txtSaldoTotal.BackColor = System.Drawing.SystemColors.Control
-        Me.txtSaldoTotal.Location = New System.Drawing.Point(1291, 630)
-        Me.txtSaldoTotal.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.txtSaldoTotal.Location = New System.Drawing.Point(968, 651)
         Me.txtSaldoTotal.MaxLength = 15
         Me.txtSaldoTotal.Name = "txtSaldoTotal"
-        Me.txtSaldoTotal.Size = New System.Drawing.Size(135, 22)
+        Me.txtSaldoTotal.Size = New System.Drawing.Size(102, 20)
         Me.txtSaldoTotal.TabIndex = 221
         Me.txtSaldoTotal.Text = "0.00"
         Me.txtSaldoTotal.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
@@ -249,22 +234,21 @@ Partial Class Frm_Contabilidad_Auxiliar_Mayor
         Me.Grid.DefaultFont = New System.Drawing.Font("Tahoma", 8.25!)
         Me.Grid.DefaultRowHeight = CType(24, Short)
         Me.Grid.FixedRowColStyle = FlexCell.FixedRowColStyleEnum.VisualStyles
-        Me.Grid.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Grid.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Grid.GridColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
-        Me.Grid.Location = New System.Drawing.Point(16, 194)
+        Me.Grid.Location = New System.Drawing.Point(12, 158)
         Me.Grid.LockButton = True
-        Me.Grid.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Grid.Name = "Grid"
         Me.Grid.Rows = 20
-        Me.Grid.Size = New System.Drawing.Size(1439, 428)
+        Me.Grid.Size = New System.Drawing.Size(1079, 487)
         Me.Grid.TabIndex = 222
         Me.Grid.UncheckedImage = CType(resources.GetObject("Grid.UncheckedImage"), System.Drawing.Bitmap)
         '
         'Frm_Contabilidad_Auxiliar_Mayor
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1471, 682)
+        Me.ClientSize = New System.Drawing.Size(1103, 674)
         Me.Controls.Add(Me.Grid)
         Me.Controls.Add(Me.txtSaldoTotal)
         Me.Controls.Add(Me.txtTotalAbonos)
@@ -272,7 +256,6 @@ Partial Class Frm_Contabilidad_Auxiliar_Mayor
         Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.ToolStrip1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
-        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.MaximizeBox = False
         Me.Name = "Frm_Contabilidad_Auxiliar_Mayor"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
