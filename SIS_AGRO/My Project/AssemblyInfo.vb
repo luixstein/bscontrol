38adaf818e777385b461cf2cb4472f38df20d320
Jorge Gonzalez <luixstein@gmail.com>
2022-01-06 13:10:41 -0700

    454 cancelación 2022 v1

--- SIS_AGRO/My Project/AssemblyInfo.vb
+++ SIS_AGRO/My Project/AssemblyInfo.vb
@@ -12,7 +12,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("")> 
 <Assembly: AssemblyProduct("BsControl")>
-<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
@@ -31,7 +31,7 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.452")> 
-<Assembly: AssemblyFileVersion("1.0.0.452")> 
+<Assembly: AssemblyVersion("1.0.0.454")>
+<Assembly: AssemblyFileVersion("1.0.0.454")>
 
 
