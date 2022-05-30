86a3dccfeadd073cc76e110ce856cb2b173b869f
Jorge Gonzalez <luixstein@gmail.com>
2022-05-30 14:10:41 -0600

    Fix menor tipocambio

--- SIS_AGRO/Modulos/FacturacionElectronica40.vb
+++ SIS_AGRO/Modulos/FacturacionElectronica40.vb
@@ -1364,7 +1364,7 @@ Module FacturacionElectronica40
                         oPagoDetalle.FACTURA_SERIE,
                         oPagoDetalle.FACTURA_FOLIO_NUMERICO,
                         oPagoDetalle.CODIGO_MONEDA_SAT_DR,
-                        IIf(oPagoDetalle.TIPO_CAMBIO_DR = CDec("1"), "1", FormatTipoCambio(oPagoDetalle.TIPO_CAMBIO_DR)).ToString,
+                        IIf(oPagoDetalle.TIPO_CAMBIO_DR = CDec("1"), "1", FormatTipoCambio(oPagoDetalle.TIPO_CAMBIO_DR, False, 6)).ToString,
                         oPagoDetalle.NUMERO_PARCIALIDAD,
                         Format(oPagoDetalle.IMPORTE_SALDO_ANTERIOR, "#0.00"),
                         Format(oPagoDetalle.IMPORTE_PAGADO, "#0.00"),
