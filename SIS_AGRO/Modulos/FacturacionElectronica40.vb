badb31267ecac8e4a3eab07b37bc520627a16e00
jorge <luixstein@gmail.com>
2024-02-03 09:15:57 -0700

    Revert "Fix INE"
    This reverts commit 2b139e5d2c244f2fd74db041174839251abc60bd.

--- SIS_AGRO/Modulos/FacturacionElectronica40.vb
+++ SIS_AGRO/Modulos/FacturacionElectronica40.vb
@@ -426,7 +426,7 @@ Module FacturacionElectronica40
             Dim oComplementoINEGlobal As New Class_ComplementoINE_Global(oVenta.FOLIO_VENTA)
             Dim complementoINE11 As New cComplementoINE11
 
-            If oComplementoINEGlobal.Existe = True Then
+            If oComplementoINEGlobal.Existe Then
 
                 Dim oProcesos As New Class_INE_CatTipoProcesos(oComplementoINEGlobal.CODIGO_PROCESO)
                 Dim oComites As New Class_INE_CatTipoComites(oComplementoINEGlobal.CODIGO_COMITE)
@@ -444,8 +444,6 @@ Module FacturacionElectronica40
 
                     If dtEntidades.Rows.Count > 0 Then
                         For Each row As DataRow In dtEntidades.Rows
-                            Contabilidades = New cINEContabilidades
-
                             dtContabilidadesDetalle = New DataTable
                             dtContabilidadesDetalle = oComplementoINEGlobal.ObtenerDetalleContabilidadesParaComplemento(CInt(row("ID_DETALLE_ENTIDADES").ToString))
 
@@ -454,7 +452,9 @@ Module FacturacionElectronica40
                             Next
 
                             .Entidades.Add(row("CODIGO_ENTIDAD").ToString, row("NOMBRE_AMBITO").ToString, Contabilidades)
+
                         Next
+
                     End If
 
                 End With
