8c63c4c75d7edf6b24333d661f4d76f560189ccd
jorge <luixstein@gmail.com>
2024-02-03 10:12:19 -0700

    479

--- SIS_AGRO/My Project/AssemblyInfo.vb
+++ SIS_AGRO/My Project/AssemblyInfo.vb
@@ -12,7 +12,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("")> 
 <Assembly: AssemblyProduct("BsControl")>
-<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
@@ -31,7 +31,7 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.477")> 
-<Assembly: AssemblyFileVersion("1.0.0.477")> 
+<Assembly: AssemblyVersion("1.0.0.479")>
+<Assembly: AssemblyFileVersion("1.0.0.479")>
 
 
